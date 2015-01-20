bb3c451c5b2daae969ad7bbb8549ecae00e69944
miles-h <miles@cyber-rights.net>
2015-01-20 07:10:22 +0000

    ...

--- room.c
+++ room.c
@@ -4,18 +4,19 @@
 
 float floor_area(float ax, float ay);
 float room_volume(float vx, float vy, float vz);
-float paint_needed(float px, float py, float pz, float coverage, float coats);
+float paint_needed(float px, float py, float pz, float obst, float coverage, float coats);
 
 /* --- Main --- */
 
 int main()
 {
-	char ret;
+	char ret; //variable for yes loops
 
 	do
 	{
 		int number;
 		float x, y, z, coverage, coats;
+		float obst = 1;
 
 		//print
 		printf("\nThis program will calculate (assuming rectangluar cuboid):\n\n1. Floor Area\n\n2. Room Volume\n\n3. Paint Required\n\n");
@@ -27,18 +28,20 @@ int main()
 		{
 			case '1':
 
+			//Dimentions
 			printf("\nEnter Floor Length(m):\n");
 			scanf("%f",&x);
-
 			printf("\nEnter Floor Width(m):\n");
 			scanf("%f",&y);
 
+			//Final Output
 			printf("\nThe area of the floor (to the nearest cm):\n%.2fm^2", floor_area(x, y));
 
 			break;
 
 			case '2':
 
+			//Dimentions
 			printf("\nEnter Floor Length(m):\n");
 			scanf("%f",&x);
 			printf("\nEnter Floor Width(m):\n");
@@ -46,12 +49,14 @@ int main()
 			printf("\nEnter Room Height(m):\n");
 			scanf("%f",&z);
 
+			//Final Output
 			printf("\nThe volume of the room (to the nearest cm):\n%.2fm^3", room_volume(x, y, z)); 
 
 			break;
 
 			case '3':
 
+			//Dimentions
 			printf("\nEnter Floor Length(m):\n");
 			scanf("%f",&x);
 			printf("\nEnter Floor Width(m):\n");
@@ -59,18 +64,57 @@ int main()
 			printf("\nEnter Room Height(m):\n");
 			scanf("%f",&z);
 
-			printf("\nEnter coverage of paint(m^2/L) (average = 9.1m^2/L)\n");
+			//Other Obsitcles (Optional)
+			printf("\nDo you need to take account of obstacles (e.g Doors)?\nEnter 'y' for yes:\n");
+			while( getchar() != '\n'){            } //input buffer flush
+			scanf ("%c",&ret);
+			if (ret == 'y' || ret == 'Y')
+			{
+				int i,j,k;
+				float temp_ox,temp_oy,ox,oy;
+				printf("\nHow many?:\n");
+				scanf("%d",&i);
+				k = i;
+				j = 0;
+				temp_ox = temp_oy = ox = oy = 0;
+
+				while(k != 0)
+				{
+					j++;
+					printf("\nEnter dimensions(m) of no. %d:\n",j);
+					printf("Height:\n");
+					scanf("%f",&temp_ox);
+					printf("\nWidth:\n");
+					scanf("%f",&temp_oy);
+
+					ox = ox + temp_ox;
+					oy = oy + temp_oy;
+
+					k--;
+
+				}
+
+				obst = ox*oy;	//total to subtract from surface area
+			}
+			else
+			{
+				obst = 0;	//total to subtract from surface area
+			}
+
+			printf("\nEnter coverage of paint(m^2/L) (average = 15m^2/L):\n");
 			scanf("%f",&coverage);
 			while( getchar() != '\n'){            } //input buffer flush
-			printf("\nEnter Number of Coatings\n");
+			printf("\nEnter Number of Coatings:\n");
 			scanf("%f",&coats);
 
-			printf("\nThe amount of paint required (to the nearist ml): \n%.3fL", paint_needed(x, y, z, coverage, coats ));
+			//Final Output
+			printf("\nThe amount of paint required (to the nearist ml): \n%.3fL", paint_needed(x, y, z, obst, coverage, coats ));
 
 			break;
-			//case 'default':
+
 		}
 
+		//Restart program prompt
 		printf("\n\nEnter 'y' to return to menu\n");
 		while( getchar() != '\n'){            } //input buffer flush
 		scanf("%c",&ret);
@@ -104,12 +148,14 @@ float room_volume(float x, float y, float z)
 
 }
 
-float paint_needed(float x, float y, float z, float coverage, float coats)
+float paint_needed(float x, float y, float z, float obst, float coverage, float coats)
 {
 
 	float paint;
 
-	paint = ((2 * x * z)+(2 * y * z));
+	paint = (x * z)+(y * z);
+	paint = paint * 2;
+	paint = paint - obst;
 	paint = paint / coverage;
 	paint = paint * coats;
 
